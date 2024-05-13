df14b5443f48640f7074a7e238cff7083723c1df
FaizanHaider108 <fiver1faizan@gmail.com>
2024-05-13 03:40:50 -0700

    Sidebar and MobileNavbar Complete

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,15 +1,26 @@
+import Image from "next/image";
 import Sidebar from "./components/Sidebar";
+import MobileNav from "./components/MobileNav";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const user = {firstName : "Faizan", lastName: "Haider"}
+  const loggedIn = {firstName : "Faizan", lastName: "Haider"}
   return (
     <main className="flex h-screen w-full font-inter">
-      <Sidebar user={user}/>
+    <Sidebar user={loggedIn} />
+
+    <div className="flex size-full flex-col">
+      <div className="root-layout">
+        <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+        <div>
+          <MobileNav user={loggedIn} />
+        </div>
+      </div>
       {children}
-   </main>
+    </div>
+  </main>
   );
 }
