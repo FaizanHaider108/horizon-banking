3a453425cb46f7405c39e88add25dd1af4f474fb
FaizanHaider108 <fiver1faizan@gmail.com>
2024-05-13 04:42:12 -0700

    Rightside of Home Screen Complete

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,23 +1,28 @@
 import React from 'react'
-import HeaderBox from './components/headerBox'
-import TotalBalanceBox from './components/TotalBalanceBox'
+import HeaderBox from '../components/headerBox'
+import TotalBalanceBox from '../components/TotalBalanceBox'
+import RigthSidebar from '../components/RigthSidebar'
+
 
 
 function Home() {
-  const loggedIn  = {firstname: "Faizan Haider"}
+  const loggedIn  = {firstName: "Faizan", lastName:"Haider", email:"info@faizanhaiderdev.online"}
   return (
     <div className="home">
       <div className="home-content">
         <header className='home-header'>
-          <HeaderBox 
+          <HeaderBox
           type="greeting" 
           title = "Welcome" 
-          user={loggedIn?.firstname || 'Guest'}
+          user={loggedIn?.firstName || 'Guest'}
           subtext="Access and manage your account and transactions efficiently"
           />
           <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1250.25}/>
         </header>
+        Recent Transections
       </div>
+      <RigthSidebar user={loggedIn} transections={[]} 
+      banks={[{currentBalance: 123.50}, {currentBalance: 123.50}]}/>
     </div>
   )
 }
