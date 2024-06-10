d2aeb78024309170c6074ab08fc8f5c3cb437885
FaizanHaider108 <fiver1faizan@gmail.com>
2024-06-11 00:15:00 +0500

    implement plaid link and Dwalla

--- app/components/Authform.tsx
+++ app/components/Authform.tsx
@@ -16,6 +16,7 @@ import { AuthformSchema } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { signIn, signUp } from "@/lib/actions/user.actions";
+import PlaidLink from "./PlaidLink";
 
 
 const Authform = ({ type }: { type: string }) => {
@@ -36,8 +37,22 @@ const Authform = ({ type }: { type: string }) => {
     setLoading(true)
     try {
       // sign up to appwrite & create plaid Token
+      
       if(type === 'sign-up'){
-        const newUser = await signUp(data)
+        const userData = {
+          firstName: data.firstName!,
+          lastName: data.lastName!,
+          address1: data.address1!,
+          city: data.city!,
+          state: data.state!,
+          postalCode: data.postalCode!,
+          dateOfBirth: data.dateOfBirth!,
+          ssn: data.ssn!,
+          email: data.email,
+          password: data.password
+        }
+
+        const newUser = await signUp(userData)
         setUser(newUser)
       }
       if(type === 'sign-in'){
@@ -84,8 +99,10 @@ const Authform = ({ type }: { type: string }) => {
         </div>
       </header>
       {user ? (
-        <div className="flex flex-col gap-4">{/*Plaid Links  */}</div>
-      ) : (
+        <div className="flex flex-col gap-4">
+          <PlaidLink user={user} variant="primary"/>
+        </div>
+       ) : ( 
         <>
           <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -144,7 +161,7 @@ const Authform = ({ type }: { type: string }) => {
         </Link>
     </footer>
         </>
-      )}
+     )}
     </section>
   );
 };
